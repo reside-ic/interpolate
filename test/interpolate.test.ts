8f6e018c5575b588396b197dc9e66df2bd7d9c0b
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-06-30 09:30:47 +0100

    Start testing

--- test/interpolate.test.ts
+++ test/interpolate.test.ts
@@ -2,9 +2,24 @@ import {interpolateSearch} from "../src/interpolate";
 
 // look in odin:tests/testthat/test-js-support-interpolate.R for test cases
 describe("interpolateSearch can find points", () => {
-    const x = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5];
+
     it("Can find point", () => {
-        const res = interpolateSearch(0, x, 0);
-        expect(res).toEqual(-1);
+        const x = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5];
+        expect(interpolateSearch(0,  x, 0)).toEqual(-1);
+        expect(interpolateSearch(10, x, 0)).toEqual(10);
+        expect(interpolateSearch(0,  x, 5)).toEqual(-1);
+        expect(interpolateSearch(10, x, 5)).toEqual(10);
+        expect(interpolateSearch(0,  x, 9)).toEqual(-1);
+        expect(interpolateSearch(10, x, 9)).toEqual(10);
+    });
+
+    it("Can find points in trivial vector", () => {
+        const x = 0.5;
+        expect(interpolateSearch(0,        [x], 0)).toEqual(-1);
+        expect(interpolateSearch(x,        [x], 0)).toEqual(1);
+        expect(interpolateSearch(x + 1e-7, [x], 0)).toEqual(1);
+        // The previous interface satisfied this, but I think this
+        // might have been a serialisation issue:
+        // expect(interpolateSearch(x - 1e-7, [x], 0)).toEqual(1);
     });
 });
